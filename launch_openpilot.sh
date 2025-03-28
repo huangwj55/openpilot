cdfd5e49e66ff0f1ccb94e5d827cb9342c23c797
huangwj55 <97580125+huangwj55@users.noreply.github.com>
2025-03-28 21:51:05 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -2,7 +2,8 @@
 # 设置 Mapbox 公钥和密钥
 echo "pk.eyJ1IjoibWF3ZWl5dXdlaXdlaSIsImEiOiJjbGtrdjhhMHUwbWwzM3VwYzVtbXkwY2lrIn0.-u-QP8-tfrhAIpvQiNMsOw" > /data/params/d/MapboxPublicKey
 echo "sk.eyJ1IjoibWF3ZWl5dXdlaXdlaSIsImEiOiJjbG15NHN1dDMwdWc5MmxwaDdkZ3Z5dHNyIn0.pk06qTKkAZyBC1Z37v8i0A" > /data/params/d/MapboxSecretKey
-
+export ATHENA_HOST=wss://athena.konik.ai
+export API_HOST=https://api.konik.ai
 # 设置适当的权限
 chmod 644 /data/params/d/MapboxPublicKey
 chmod 644 /data/params/d/MapboxSecretKey
